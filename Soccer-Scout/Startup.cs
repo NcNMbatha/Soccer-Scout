bf119c8d4563ebd850cbc66dafc608517d0dddb1
NcNMbatha <mbathann45@gmail.com>
2021-03-20 20:45:04 +0200

    initial commit

---
+++ Soccer-Scout/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Soccer_Scout.Startup))]
+namespace Soccer_Scout
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
